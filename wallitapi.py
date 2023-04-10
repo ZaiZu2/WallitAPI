060a355f20ecefe97519faccb56511b25a33b5bb
ZaiZu2 <jakkawecki@gmail.com>
2023-04-10 14:29:14 +0200

    new: logging added: 1. Response logs are now streamed and written to log file 2. Errors and info are now streamed and written to a log file

--- wallitapi.py
+++ wallitapi.py
@@ -1,7 +1,9 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
+from pathlib import Path
 
+from config import LOGGING_CONFIG
 from api import user, transactions, categories, tags_metadata
 from api.error_handlers import request_validation_handler
 
@@ -12,12 +14,14 @@ def create_app() -> FastAPI:
     app.include_router(user.router)
     app.include_router(transactions.router)
     app.include_router(categories.router)
-    
+
     app.add_exception_handler(RequestValidationError, request_validation_handler)
+
+    Path("./logs").mkdir(exist_ok=True)
 
     return app
 
 
 app = create_app()
 if __name__ == "__main__":
-    uvicorn.run(app)
+    uvicorn.run(app, log_config=LOGGING_CONFIG)
